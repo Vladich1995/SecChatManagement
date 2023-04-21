0a2e5d10fc19b715792b8c54781e426c4b06f183
Vladich1995 <vlad.charny@gmail.com>
2023-04-21 06:07:08 +0300

    updated path for express

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require("../node_modules/express");
 const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
